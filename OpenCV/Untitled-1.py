5e173061ab9895ab6356feeed20f724e03ab757f
fetrentin7 <fetrentin7@gmail.com>
2025-11-16 23:21:37 -0300

    opencv

--- OpenCV/Untitled-1.py
+++ OpenCV/Untitled-1.py
@@ -1,1 +1,33 @@
-print('aaa')
+import cv2
+
+# Load the desired classifier
+# For profile faces, use 'haarcascade_profileface.xml'
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+
+# For video capture
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        break
+
+    # Convert frame to grayscale as classifiers work on grayscale images
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # Detect faces
+    # adjust parameters (scaleFactor, minNeighbors) for better results
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=4, minSize=(30, 30))
+
+    # Draw rectangles around the detected faces
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
+
+    # Display the result
+    cv2.imshow('Face Detection (Non-Frontal might be limited)', frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
