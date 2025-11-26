4a6b876b02590bf58fb2ff623021758e422bc1ec
fetrentin7 <fetrentin7@gmail.com>
2025-11-26 14:57:12 -0300

    camera

--- OpenCV/Untitled-1.py
+++ OpenCV/Untitled-1.py
@@ -1,10 +1,9 @@
 import cv2
 
 # Load the desired classifier
-# For profile faces, use 'haarcascade_profileface.xml'
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
-# For video capture
+#video capture
 cap = cv2.VideoCapture(0)
 
 while True:
@@ -20,13 +19,17 @@ while True:
     faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=4, minSize=(30, 30))
 
     # Draw rectangles around the detected faces
+
+    #definir setpoint, deslocamento
+    #quando se afastar, mensaagem para se aproximar, mesma coisa para a inclinação da camera
+    setpoint = 0
     for (x, y, w, h) in faces:
         cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
 
     # Display the result
     cv2.imshow('Face Detection (Non-Frontal might be limited)', frame)
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    if cv2.waitKey(1) & 0xFF == ord('esc'):
         break
 
 cap.release()
